940aa516a988f7b7cb237dbe03a8c83bbc9985b5
David Barrow <dave@fusion-arc.uk>
2021-07-03 21:21:20 +0100

    working prototype updated from old repo

---
+++ saulgadgets/settings.py
@@ -0,0 +1,130 @@
+import os
+from getenv import env
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+PAYPAL_API_KEY_HIDDEN = env('PAYPAL_API_KEY_HIDDE')
+PAYPAL_API_KEY_PUBLISHABLE = env('PAYPAL_API_KEY_PUBLISHABLE')
+
+RAZORPAY_API_KEY_HIDDEN = env('RAZORPAY_API_KEY_HIDDEN')
+RAZORPAY_API_KEY_PUBLISHABLE = env('RAZORPAY_API_KEY_PUBLISHABLE')
+
+STRIPE_API_KEY_PUBLISHABLE =  env('STRIPE_API_KEY_PUBLISHABLE')
+STRIPE_API_KEY_HIDDEN = env('STRIPE_API_KEY_HIDDEN')
+
+# Cart
+
+SESSION_COOKIE_AGE = 86400
+CART_SESSION_ID = 'cart'
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'apps.cart',
+    'apps.coupon',
+    'apps.core',
+    'apps.order',
+    'apps.store',
+    'apps.userprofile',
+    'apps.newsletter'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'saulgadgets.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'apps.store.context_processors.menu_categories',
+                'apps.cart.context_processors.cart'
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'saulgadgets.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
